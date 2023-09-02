05c6dd49ed79939a78ce0b56061d6d33047dff32
Ermal Mamaj <ermalmamaj@gmail.com>
2023-09-02 11:32:15 +0200

    TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -27,7 +27,11 @@ while repeate.lower() == "y":
             board[choice - 1] = icon
         else:
             print()
-            print("That space is already taken!")
+            print("That space is already taken!\n")
+            choice = int(input("Enter your move (1-9): ").strip())
+
+            if board[choice - 1] == ' ':
+                board[choice - 1] = icon
 
     def is_victory(icon):
         if (board[0] == icon and board[1] == icon and board[2] == icon) or \
